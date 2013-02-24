70188a36f04ebaec4e560a1cf3a8e2591951124e
James <e@mailjames.com>
2013-02-24 21:37:02 +0000

    basic shell

---
+++ library/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class Model
+    {
+        public string AppName { get; set; }
+        public SubModel SubModel { get; set; }
+    }
+
+    public class SubModel
+    {
+        public string Path { get; set; } 
+        public string Command { get; set; } 
+    }
+}
