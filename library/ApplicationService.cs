a66bda36d148e2300fe81a148af40bb781c6fc4c
James <e@mailjames.com>
2013-02-24 21:38:22 +0000

    basic shell

--- library/ApplicationService.cs
+++ library/ApplicationService.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace library
 {
-    class Serialise
+    public class ApplicationService
     {
+        public string JsonPath { get; set; }
+        public List<Model> Applications { get; set; }
+ 
+        public ApplicationService(string jsonPath)
+        {
+            JsonPath = jsonPath;
+        }
+
+        public void Read()
+        {
+            var StreamReader = new StreamReader(JsonPath);
+            var output = StreamReader.ReadToEnd();
+            StreamReader.Close();
+
+            Applications = JsonConvert.DeserializeObject<List<Model>>(output);
+
+        }
+
+        public void StartApplications()
+        {
+            foreach (var application in Applications)
+            {
+                
+            }
+        }
     }
 }
